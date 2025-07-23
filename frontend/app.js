d47dfd5db48d8bd5b01c4b9870d14b6b7970fd22
roshni9966 <081bei027.roshani@pcampus.edu.np>
2025-07-23 18:23:57 +0545

    helo

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     // POST to FastAPI
-    const res = await fetch(`http://localhost:8000${endpoint}`, {
+    const res = await fetch(`https://ajax-demo-6cx8.vercel.app${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
